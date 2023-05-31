7e50bd589b468b5db7f709750883bad2b4df740f
Rodion <rodion_petrenko@list.ru>
2023-05-31 18:09:23 +0300

    Исправил замечяние по тексту

--- test/src/components/tets-count.jsx
+++ test/src/components/tets-count.jsx
@@ -7,7 +7,7 @@ const [count, setCount] = useState(0)
     return(
         <div>
             <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Пельмени</button>
+            <button onClick={() => setCount(count + 1)}>авокадо</button>
         </div>
     )
 }
